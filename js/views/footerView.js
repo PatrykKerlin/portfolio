970e3fe461521d39f37334c2947f4c884deb060f
PatrykKerlin <patryk.kerlin@gmail.com>
2022-10-01 18:31:30 +0200

    Working on vertical responsiveness

--- js/views/footerView.js
+++ js/views/footerView.js
@@ -2,8 +2,15 @@ import View from './View.js';
 
 class FooterView extends View {
 	_footer = document.querySelector('.footer');
+	_year = document.querySelector('.year');
+
+	_setYear() {
+		const currentYear = new Date().getFullYear();
+		this._year.textContent = currentYear;
+	}
 
 	showFooterView() {
+		this._setYear();
 		this._slideUp(this._footer, 'linear', 2000, 1500);
 	}
 }
--- js/views/headerView.js
+++ js/views/headerView.js
@@ -2,16 +2,19 @@ import View from './View.js';
 
 class HeaderView extends View {
 	_header = document.querySelector('.header');
+	_btnMobileNav = document.querySelector('.btn-mobile-nav');
+
+	_toggleBtnMobileNav() {
+		this._header.classList.toggle('nav-open');
+	}
 
 	showHeaderView() {
 		this._slideDown(this._header, 'linear', 2000, 1500);
 
-		const btnNavEl = document.querySelector('.btn-mobile-nav');
-		const headerEl = document.querySelector('.header');
-
-		btnNavEl.addEventListener('click', function () {
-			headerEl.classList.toggle('nav-open');
-		});
+		this._btnMobileNav.addEventListener(
+			'click',
+			this._toggleBtnMobileNav.bind(this)
+		);
 	}
 }
 
