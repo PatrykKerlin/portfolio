92046ac9e6e3f87ce0abaa01b12fe989d42068b3
PatrykKerlin <patryk.kerlin@gmail.com>
2022-10-01 13:15:30 +0200

    Added mobile navigation

--- js/views/headerView.js
+++ js/views/headerView.js
@@ -5,6 +5,13 @@ class HeaderView extends View {
 
 	showHeaderView() {
 		this._slideDown(this._header, 'linear', 2000, 1500);
+
+		const btnNavEl = document.querySelector('.btn-mobile-nav');
+		const headerEl = document.querySelector('.header');
+
+		btnNavEl.addEventListener('click', function () {
+			headerEl.classList.toggle('nav-open');
+		});
 	}
 }
 
